a1cf2510bc71050a9fdcbbe8b5854dc599ea6af0
Tiago Quadros <97044147+tiago-sq@users.noreply.github.com>
2023-01-18 18:51:22 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,30 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
 describe("Teste do App Jokes", () => {
-  test('exibe o título "Piadas do Chuck Norris" na tela', () => {
+  test('Exibe o título "Piadas do Chuck Norris" na tela', () => {
     render(<App />);
-
+    // const titleEl = screen.getByText(/piadas do chuck norris/i);
+    const titleEl = screen.getByRole("heading", {
+      name: /piadas do chuck norris/i
+    });
+    expect(titleEl).toBeInTheDocument();
   });
 
-  test("exibe o corpo da piada na tela", async () => {
+  test("Exibe o corpo da piada na tela", async () => {
+    // coloque o código
+    const joke = {
+      value: "Chuck Norris died"
+    };
 
+    jest.spyOn(global, "fetch");
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(joke)
+    });
+
+    render(<App />);
+    const jokeEl = await screen.findByText(/Chuck Norris died/i);
+    expect(jokeEl).toBeInTheDocument();
   });
 });
